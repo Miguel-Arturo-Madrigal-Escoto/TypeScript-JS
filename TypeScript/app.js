2909391119978b36973ba8b5de13f5579b2ed19e
unknown <miguel.madrigal7676@alumnos.udg.mx>
2021-03-28 17:36:20 -0600

    usando advertencias el ts-js

--- TypeScript/app.js
+++ TypeScript/app.js
@@ -3,3 +3,6 @@ var numero = 10;
 console.log('Hola Mundo');
 console.log('Este es desde TypeScript');
 console.log('El numero es: ' + numero);
+// usando una advertencia
+console.warn('Cierra la ventana plox');
+console.error('Close the window pls');
--- TypeScript/app.ts
+++ TypeScript/app.ts
@@ -4,3 +4,7 @@ console.log('Hola Mundo');
 console.log('Este es desde TypeScript');
 console.log('El numero es: ' + numero);
 
+// usando una advertencia
+console.warn('Cierra la ventana plox');
+console.error('Close the window pls');
+
